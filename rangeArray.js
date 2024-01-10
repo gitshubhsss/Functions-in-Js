69725f59d1437de168a1b856e9307be4f36fc2b7
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-10 21:16:00 +0530

    range between two elements

---
+++ rangeArray.js
@@ -0,0 +1,13 @@
+/* 25)Write a arrow function to generate an array between two integers of 1 step length.
+    console.log(rangeBetwee(4, 7));
+    OUTPUT :[4, 5, 6, 7]
+    console.log(rangeBetwee(-4, 7));
+    OUTPUT :[-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7] */
+let arr = [];
+let rangeBetween = (start, end) => {
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+console.log(rangeBetween(4, 7));
